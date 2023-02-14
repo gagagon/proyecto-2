ab7ad2a975c0649fa7fa61695deeb563c75cdbf5
gagagon <124789502+gagagon@users.noreply.github.com>
2023-02-14 11:28:00 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,32 @@
+input.onButtonPressed(Button.A, function () {
+    if (input.buttonIsPressed(Button.A)) {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+let puntos = 0
+puntos += 0
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(200)
+    basic.showIcon(IconNames.House)
+    basic.pause(200)
+    basic.showIcon(IconNames.Yes)
+    basic.pause(200)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.pause(200)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.showIcon(IconNames.Angry)
+    basic.pause(200)
+    basic.showIcon(IconNames.Skull)
+    basic.pause(200)
+    basic.showIcon(IconNames.No)
+    basic.pause(200)
+    basic.showIcon(IconNames.Rollerskate)
+    basic.pause(200)
+    basic.showIcon(IconNames.Rabbit)
+    basic.pause(200)
+})
