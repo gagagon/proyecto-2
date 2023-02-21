1f402b70390afc0d427cc218555693da95e7b292
gagagon <124789502+gagagon@users.noreply.github.com>
2023-02-21 10:56:41 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,59 @@
 input.onButtonPressed(Button.A, function () {
-    if (input.buttonIsPressed(Button.A)) {
-    	
+    if (pulsado == false) {
+        pulsado = true
+        if (num_icono == 0 || num_icono == 2) {
+            puntos_A += 1
+        } else if (num_icono == 0) {
+            basic.showString("A: has perdido")
+            basic.pause(5000)
+        } else {
+            if (puntos_A > 0) {
+                puntos_A += -1
+            }
+        }
     }
 })
-input.onButtonPressed(Button.B, function () {
+function partida_nueva () {
 	
+}
+input.onButtonPressed(Button.B, function () {
+    if (pulsado == false) {
+        pulsado = true
+        if (num_icono == 0 || num_icono == 2) {
+            puntos_B += 1
+        } else if (num_icono == 0) {
+            basic.showString("A: has perdido")
+            basic.pause(5000)
+        } else {
+            if (puntos_B > 0) {
+                puntos_B += -1
+            }
+        }
+    }
 })
-let puntos = 0
-puntos += 0
+function cambia_icono () {
+    icono = randint(0, 9)
+    lista[icono].showImage(0)
+    basic.pause(200)
+}
+let icono = 0
+let puntos_B = 0
+let puntos_A = 0
+let num_icono = 0
+let pulsado = false
+let lista: Image[] = []
+lista = [
+images.iconImage(IconNames.Heart),
+images.iconImage(IconNames.StickFigure),
+images.iconImage(IconNames.Happy),
+images.iconImage(IconNames.Asleep),
+images.iconImage(IconNames.Angry),
+images.iconImage(IconNames.Yes),
+images.iconImage(IconNames.Sad),
+images.iconImage(IconNames.Ghost),
+images.iconImage(IconNames.Confused),
+images.iconImage(IconNames.Skull)
+]
 basic.forever(function () {
-    basic.showIcon(IconNames.Happy)
-    basic.pause(200)
-    basic.showIcon(IconNames.House)
-    basic.pause(200)
-    basic.showIcon(IconNames.Yes)
-    basic.pause(200)
-    basic.showIcon(IconNames.SmallSquare)
-    basic.pause(200)
-    basic.showIcon(IconNames.Heart)
-    basic.pause(200)
-    basic.showIcon(IconNames.Angry)
-    basic.pause(200)
-    basic.showIcon(IconNames.Skull)
-    basic.pause(200)
-    basic.showIcon(IconNames.No)
-    basic.pause(200)
-    basic.showIcon(IconNames.Rollerskate)
-    basic.pause(200)
-    basic.showIcon(IconNames.Rabbit)
-    basic.pause(200)
+    cambia_icono()
 })
