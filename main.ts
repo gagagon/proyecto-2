3d7ebd5174e4c007f7388c20e47271315756964d
gagagon <124789502+gagagon@users.noreply.github.com>
2023-02-21 11:28:17 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,8 @@ input.onButtonPressed(Button.A, function () {
         if (num_icono == 0 || num_icono == 2) {
             puntos_A += 1
             if (puntos_A == 10) {
-            	
+                basic.showString("A: has ganado")
+                juego_detenido = true
             }
         } else if (num_icono == 0) {
             basic.showString("A: has perdido")
@@ -17,20 +18,25 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 function partida_nueva () {
-    primero = false
+    juego_detenido = false
     puntos_A = 0
     puntos_B = 0
+    pulsado = false
 }
+input.onButtonPressed(Button.AB, function () {
+    partida_nueva()
+})
 input.onButtonPressed(Button.B, function () {
     if (pulsado == false) {
         pulsado = true
         if (num_icono == 0 || num_icono == 2) {
             puntos_B += 1
             if (puntos_B == 10) {
-            	
+                basic.showString("B: has ganado")
+                juego_detenido = true
             }
         } else if (num_icono == 0) {
-            basic.showString("A: has perdido")
+            basic.showString("B: has perdido")
             basic.pause(5000)
         } else {
             if (puntos_B > 0) {
@@ -46,7 +52,7 @@ function cambia_icono () {
 }
 let icono = 0
 let puntos_B = 0
-let primero = false
+let juego_detenido = false
 let puntos_A = 0
 let num_icono = 0
 let pulsado = false
@@ -63,6 +69,9 @@ images.iconImage(IconNames.Ghost),
 images.iconImage(IconNames.Confused),
 images.iconImage(IconNames.Skull)
 ]
+partida_nueva()
 basic.forever(function () {
-    cambia_icono()
+    if (!(juego_detenido)) {
+        cambia_icono()
+    }
 })
